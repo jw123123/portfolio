1cb89d128b186bba440617c464d0418f7b400c53
jiwon <mimi1408@naver.com>
2026-06-18 07:20:16 +0900

    푸터 및 코드 정리

--- common.js
+++ common.js
@@ -1,13 +1,37 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+  // 헤더
+  window.addEventListener('scroll', () => {
+  const header = document.querySelector('#hd');
+
+  if (window.scrollY > 1080) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
+
 // 프로젝트 영역 GSAP
 gsap.registerPlugin(ScrollTrigger);
 
 ScrollTrigger.create({
-  trigger: '.project-section',
+  trigger: '.project-container',
   pin: '.project-bg-title',
   start: 'top center',
   end: 'bottom bottom',
   scrub: 1,
   pinSpacing: false,
 });
+
+
+// 스플래쉬화면
+ const splash = document.querySelector('.splash');
+
+ document.body.style.overflowY = 'hidden';
+ 
+ setTimeout(() => {
+    splash.classList.add('hidden');
+    document.body.style.overflow = '';
+  }, 1500);
+
 });
